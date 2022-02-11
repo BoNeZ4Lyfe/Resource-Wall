5d64f8b9676940fca029b228d5201bd46a8e16b6
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2022-02-11 16:46:07 -0500

    Add database tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,46 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS user_likes CASCADE;
+DROP TABLE IF EXISTS resource_comments CASCADE;
+DROP TABLE IF EXISTS topics CASCADE;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  about_me TEXT,
+
+);
+
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  topic_id INTEGER REFERENCES topics(id) ON DELETE CASCADE,
+  url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  rating SMALLINT DEFAULT 0,
+  created_at TIMESTAMP,
+);
+
+CREATE TABLE user_likes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  liked_at TIMESTAMP,
+);
+
+CREATE TABLE resource_comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  comment TEXT,
+  created_at TIMESTAMP,
+);
+
+CREATE TABLE topics (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
 );
