07ed2f276dfd459e7c5f3b8f854f840617b9c193
knagayama76 <knagayama76>
2022-02-12 19:25:37 -0500

    add resource.ejs

--- routes/resource-router.js
+++ routes/resource-router.js
@@ -4,9 +4,18 @@ const router = express.Router();
 // GET / resources
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users LIMIT 5;`)
+    const userId = 1; // TODO: add session/cookie for logged in user later
+    db.query(
+      `
+    SELECT * FROM resources
+    JOIN users ON user_id = $1
+    LIMIT 5;
+    ;`,
+      [userId]
+    )
       .then((data) => {
-        res.json(data.rows);
+        // res.json(data.rows); // Not API request anymore
+        res.render("resources", { resources: data.rows }); // <---- new edit
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
