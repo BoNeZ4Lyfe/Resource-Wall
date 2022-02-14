88b533c5f61a37fce628e6e7f4991c9454130682
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2022-02-14 13:17:08 -0500

    Add new user to database/ update login/register ejs

--- routes/login-router.js
+++ routes/login-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const { userEmailLookup } = require("../public/scripts/helpers");
+const router = express.Router();
+
 
 module.exports = (db) => {
   // GET /login
--- routes/register-router.js
+++ routes/register-router.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const { addUser } = require("../public/scripts/helpers");
 const router = express.Router();
 
 // GET /register
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("register", { loggedIn: req.session.loggedIn, username: req.session.username });
+    res.render("register", {
+      loggedIn: req.session.loggedIn,
+      username: req.session.username,
+    });
+  });
+
+  router.post("/", (req, res) => {
+    const user = req.body;
+    const password = req.body.password;
+
+    if (user.password === password) {
+      addUser(user, db);
+      req.session.loggedIn = true;
+      req.session.username = user.name;
+      res.redirect("/");
+    } else {
+      return res.status(403).send("Incorrect password.");
+    }
   });
   return router;
 };
