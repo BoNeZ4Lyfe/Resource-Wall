8ccebaca609b25ea78ab4169f1b3c5acfb44d343
Gerard Compion <gcompion93@gmail.com>
2022-02-14 17:04:11 -0500

    set up routes and placeholder ejs for user-settings

---
+++ routes/user-settings-router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    if (req.session.loggedIn) {
+      const templateVars = {
+        loggedIn: req.session.loggedIn,
+        userID: req.session.userID,
+        username: req.session.username
+      };
+      res.render("user-settings", templateVars);
+    } else {
+      res.redirect("/");
+    }
+  });
+  return router;
+};
