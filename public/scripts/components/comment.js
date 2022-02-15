9a5e47ebe99abf4f98ab42f4488798635f9c0c18
knagayama76 <knagayama76>
2022-02-14 19:55:56 -0500

    add forms/buttons on /resources

--- public/scripts/components/comment.js
+++ public/scripts/components/comment.js
@@ -1,16 +1,21 @@
-addconst $newCommentForm = $(`
-  <form action="/api/resource_comment" method="post" class="new-comment-form">
-    <div class="new-comment-form">
-      <label for="new-comment">Comment</label>
-      <input type="text" name="comment" placeholder="comment" >
-    </div>
-  </form>
-`);
-
 $(() => {
-  console.log("READY");
   $(".btn--comment").on("click", () => {
-    console.log("comment btn clicked!!!");
-    $(".resource-containter").prepend($newCommentForm);
+    const classesOnComment = $("#comment-create").attr("class");
+    console.log(classesOnComment);
+
+    if (classesOnComment.includes("hide")) {
+      $("#comment-create").removeClass("hide");
+      $("#comment-create").addClass("show");
+    }
+
+    const classesOnBtn = $(".btn--new").attr("class");
+    if (classesOnBtn.includes("show")) {
+      $(".btn--new").toggle();
+    }
+
+    const classesOnResources = $(".resource-containter").attr("class");
+    if (classesOnResources.includes("show")) {
+      $(".resource-containter").toggle();
+    }
   });
 });
